ff678eaf050883e8763b79cd96c43b434bee37bf
VK0537 <veka@pm.me>
2021-01-11 11:57:39 +0100

    Revert "Update statslib.py"
    This reverts commit 693917b0af6704d2431f538e88257f39575d888e.

--- statslib.py
+++ statslib.py
@@ -5,11 +5,8 @@ class Stat():
         self.C=modalites
     def __repr__(self):
         return f"{{ {self.X.__name__} : {(self.O if len(self.O)<10 else 'pop.')} -> {(self.C if len(self.C)<10 else 'mod.')}\n{{ {' '*(len(self.X.__name__)+2)} w -> {self.X.__name__}(w)"
-    
-    def effectif(self,i):
-        return sum(1 for j in self.O if self.X(j)==self.C[i])
-def f(a):return (True if a%2==0 else False)
-newstat=Stat((1,2,3,4,5,6,7,8,9),f,(False,True))
+    def effectif(self):
+        pass
+def f(a):return (1 if a%2==0 else 0)
+newstat=Stat((1,2,3,4,5,6,7,8,9),f,(0,1))
 print(newstat)
-print(newstat.effectif(0))
-print(newstat.effectif(1))
