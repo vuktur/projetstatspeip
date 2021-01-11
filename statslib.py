a88e01691fd8be37e82967be4a8b09eef2011392
VK0537 <veka@pm.me>
2021-01-11 16:30:41 +0100

    new

--- statslib.py
+++ statslib.py
@@ -1,3 +1,6 @@
+from math import exp
+from math import sin
+
 class Stat():
     def __init__(self,population,*statistiques):
         self.O=population
@@ -5,17 +8,16 @@ class Stat():
         self.C=tuple(sorted(list(dict.fromkeys(self.all())))) #modalitées
     def __repr__(self):return f"{{ {self.X.__name__} : {(self.O if len(self.O)<10 else 'pop.')} -> {(self.C if len(self.C)<10 else 'mod.')}\n{{ {' '*(len(self.X.__name__)+2)} w -> {self.X.__name__}(w)"
     def all(self):return [self.X(i) for i in self.O]
-    def effectif(self,i):return sum(1 for j in self.O if self.X(j)==self.C[i])
-    def effectifCummule(self,i):return sum(self.effectif(j) for j in range(0,i))
+    # def effectif(self,i):return sum(1 for j in self.O if self.X(j)==self.C[i-1])
+    # def effectifCummule(self,i):return sum(self.effectif(j) for j in range(0,i+1))
+    def n(self,i):return sum(1 for j in self.O if self.X(j)==self.C[i-1])
+    def N(self,i):return sum(self.n(j) for j in range(1,i+1))
 
 # def f(x):return (True if x%2==0 else False)
-# div2=Stat((1,2,3,4,5,6,7,8,9),f)
-# print(div2)
-# print(div2.effectif(0))
-# print(div2.effectif(1))
-
-def g(x):return (x+6)%5
-exemple=Stat((1,2,3,4,5,6,7,8,9),g)
+# def g(x):return (x+6)%5
+def h(x):return abs((1/2)*(exp(x)+exp(-x))*sin(x))
+exemple=Stat( tuple([i for i in range(-5,6)]) , h )
 print(exemple.all())
 print(exemple.C)
-print(exemple.effectif(0))
+print(exemple.n(4))
+print(exemple.N(5))
