92dbc7d8884802c680ae871cefc23dc75b909354
VK0537 <veka@pm.me>
2021-01-11 12:51:25 +0100

    commit on 'viktor

--- statslib.py
+++ statslib.py
@@ -8,11 +8,13 @@ class Stat():
     def test(self):return [self.X(i) for i in self.O]
     def effectif(self,i):return sum(1 for j in self.O if self.X(j)==self.C[i])
     def effectifCummule(self,i):return sum(self.effectif(j) for j in range(0,i))
+
 def f(x):return (True if x%2==0 else False)
 div2=Stat((1,2,3,4,5,6,7,8,9),f,(True,False))
 print(div2)
 print(div2.effectif(0))
 print(div2.effectif(1))
+
 def g(x):return (x+6)%10
 exemple=Stat((1,2,3,4,5,6,7,8,9),g)
 print(exemple.test())
