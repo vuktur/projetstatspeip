a6a108f69aaf77e23e470016e0046864a609965d
VK0537 <veka@pm.me>
2021-01-12 16:24:39 +0100

    new repr

--- statslib.py
+++ statslib.py
@@ -6,9 +6,10 @@ class Stat():
     def __init__(self,population,*statistiques):
         self.O=population
         self.X=statistiques[0]
-        self.C=tuple(sorted(list(dict.fromkeys(self.all())))) #modalitées
-    def __repr__(self): return f"{{ {self.X.__name__} : {(self.O if len(self.O)<10 else 'pop.')} -> {(self.C if len(self.C)<10 else 'mod.')}\n{{ {' '*(len(self.X.__name__)+2)} w -> {self.X.__name__}(w)"
-    def all(self):      return [self.X(i) for i in self.O]
+        self.C=tuple(sorted(list(dict.fromkeys(self.serie())))) #modalitées
+    def __repr__(self): return f"valeur   |{'|'.join([str(i) for i in self.C])}\neffectif |{'|'.join([' '*(len(str(self.C[i-1]))-1)+str(self.ef(i)) for i in range(1,len(self.C)+1)])}"
+    #return f"{{ {self.X.__name__} : {(self.O if len(self.O)<10 else 'pop.')} -> {(self.C if len(self.C)<10 else 'mod.')}\n{{ {' '*(len(self.X.__name__)+2)} w -> {self.X.__name__}(w)"
+    def serie(self):    return [self.X(i) for i in self.O]
     def ef(self,i):     return sum(1 for j in self.O if self.X(j)==self.C[i-1] and i>0)
     def efC(self,i):    return sum(self.ef(j) for j in range(1,i+1))
     def fr(self,i):     return (self.ef(i)/len(self.O) if i>0 else 0)
@@ -20,7 +21,7 @@ def h(x):return abs((1/2)*(exp(x)+exp(-x))*sin(x))
 
 exemple=Stat(tuple([i for i in range(-5,6)]),h)
 
-print(exemple.all())
+print(exemple.serie())
 print(exemple.C)
 
 print(exemple.ef(4))
@@ -32,3 +33,5 @@ print(exemple.fr(4))
 print(exemple.frC(5))
 print(exemple.fr(0))
 print(exemple.frC(len(exemple.C))==1)
+
+print(exemple)
