1b1614cb2523d4ce6dca53ed14f894ef40849642
VK0537 <veka@pm.me>
2021-01-18 13:26:58 +0100

    Update statslib.py

--- statslib.py
+++ statslib.py
@@ -23,15 +23,17 @@ class Stat():
         # s=sorted(self.serie())
         # return (s[l//2] if l%2!=0 else (s[l//2]+s[l//2-1])/2)
     def quan(self,r,a=[],l=[]):
+        print(r)
         if not a:
             a=sorted(self.serie())
-        if r==1:return l
         if r%2==0:
-            m=(a[l//2] if len(a)%2!=0 else (a[l//2]+a[l//2-1])/2)
-            l.append(m)
+            m=(a[len(a)//2] if len(a)%2!=0 else (a[len(a)//2]+a[len(a)//2-1])/2)
+            l.append(float(m))
             a=[[i for i in a if i<m],[i for i in a if i>m]]
-            r-=1
-        else:pass
+            print(a,l)
+            for i in a:self.quan(r/2,i,l) if r/2!=1 else None
+        return sorted(l)
+        # else:print('odd')
     # def quan(self,r):
     #     r-=2
     #     a=[i for i in sorted(self.serie()) if i<self.med()]
@@ -43,10 +45,10 @@ class Stat():
 # def g(x):return (x+6)%5
 # def h(x):return abs((1/2)*(exp(x)+exp(-x))*sin(x))
 def l(x):return [j for i,j in enumerate([10,20,30,40]) if i==x][0]
-def m(x):return [j for i,j in enumerate([10,20,30,40,50]) if i==x][0]
+def m(x):return [j for i,j in enumerate([i*10 for i in range(50)]) if i==x][0]
 def k(x):return (x**3+3*x**2 if x<=0 else (x**(-1) if x<4 else x**2-7/2*x))
-exemple=Stat(tuple([i for i in range(0,5)]),m)
-print(exemple.serie())
+exemple=Stat(tuple([i for i in range(0,32)]),m)
+# print(exemple.serie())
 # print(exemple.C)
 # print(exemple.ef(4))
 # print(exemple.efC(5))
@@ -57,5 +59,6 @@ print(exemple.serie())
 # print(exemple.fr(0))
 # print(exemple.frC(len(exemple.C))==1)
 print(exemple)
-print(exemple.mode())
-print(exemple.med())
+# print(exemple.mode())
+# print(exemple.med())
+print(exemple.quan(4))
