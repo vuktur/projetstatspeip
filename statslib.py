5e233b301d96bac40ce9107e6855a0d85aaa1ce5
VK0537 <veka@pm.me>
2021-01-24 14:42:31 +0100

    Update statslib.py

--- statslib.py
+++ statslib.py
@@ -1,4 +1,4 @@
-# from math import exp,sin
+from math import exp,sin,sqrt
 from fractions import Fraction
 from random import randint
 import statistics
@@ -6,6 +6,7 @@ import statistics
 class Stat():
     def __init__(self,population,*statistiques):
         self.pop=population
+        self.N=len(self.pop)
         self.X=statistiques[0]
         self.C=sorted(list(dict.fromkeys(self.serie()))) #modalitées
     def __repr__(self): 
@@ -17,12 +18,12 @@ class Stat():
     def serie(self):    return [self.X(i) for i in self.pop]
     def ef(self,n):     return sum(1 for i in self.pop if self.X(i)==self.C[n-1] and n>0)
     def efC(self,n):    return sum(self.ef(i) for i in range(1,n+1))
-    def fr(self,n):     return (self.ef(n)/len(self.pop) if n>0 else 0)
+    def fr(self,n):     return (self.ef(n)/self.N if n>0 else 0)
     def frC(self,n):    return sum(self.fr(i) for i in range(1,n+1))
     def mode(self):     return [self.C[i-1] for i in range(0,len(self.C)) if self.ef(i)==max([self.ef(j) for j in range(1,len(self.C)+1)])]
     def med(self):      return self.quan(2)[0]
-    def moy(self):      return sum(i for i in self.serie())/len(self.pop)
-    def etnd(self):     return max(i for i in self.serie())-min(i for i in self.serie())
+    def moy(self):      return sum(i for i in self.serie())/self.N
+    def etendue(self):  return max(i for i in self.serie())-min(i for i in self.serie())
     # def quan(self,r):
     #     l=[]
     #     s=sorted(self.serie())
@@ -45,16 +46,27 @@ class Stat():
             # print(s[int(i*(len(s)/n)-0.5)],(s[int(i*(len(s)/n))]+s[int(i*(len(s)/n))-1])/2)
             # if (i*(len(s)/n)-0.5)%1==0: q.append(s[int(i*(len(s)/n)-0.5)])
             # else: q.append((s[int(i*(len(s)/n))]+s[int(i*(len(s)/n))-1])/2)
-            q.append(s[int(i*(len(s)/n)-0.5)] if (i*(len(s)/n)-0.5)%1==0 else (s[int(i*(len(s)/n))]+s[int(i*(len(s)/n))-1])/2)
+            # q.append(s[int(i*(len(s)/n)-0.5)] if (i*(len(s)/n)-0.5)%1==0 else (s[int(i*(len(s)/n))]+s[int(i*(len(s)/n))-1])/2)
+            pass
         return q
+    def ecartmoy(self): return sum(abs(i-self.moy()) for i in self.serie())/self.N
+    def variance(self): return sum((i-self.moy()**2) for i in self.serie())/self.N
+    def ecarttyp(self): return sqrt(self.variance())
+    def moment(self,k): return sum(i**k for i in self.serie())/self.N
+    def momentcentre(self,k): return sum((i-self.moy())**k for i in self.serie())/self.N
+
+
+
+
+
 # def f(x):return (True if x%2==0 else False)
 # def g(x):return (x+6)%5
 # def h(x):return abs((1/2)*(exp(x)+exp(-x))*sin(x))
-# z={i:randint(0,10) for i in range(100)}
+z={i:randint(0,10) for i in range(100)} 
 def m(x):return [j for i,j in enumerate([i*10 for i in range(50)]) if i==x][0]
 def k(x):return (x**3+3*x**2 if x<=0 else (x**(-1) if x<4 else x**2-7/2*x))
 def p(x):return z[x]
-# exemple=Stat(tuple([i for i in range(10)]),p)
+exemple=Stat(tuple([i for i in range(10)]),p)
 # print(exemple.pop)
 # print(exemple.serie())
 # print(sorted(exemple.serie()))
@@ -71,7 +83,7 @@ def p(x):return z[x]
 # print(exemple.mode())
 # print(exemple.med())
 # print(exemple.moy())
-# print(exemple.etnd())
+# print(exemple.etendue())
 # print(exemple.quan(4))
 # i=3
 # for j in range(2,50):
@@ -82,7 +94,7 @@ def p(x):return z[x]
 #         print(sorted(exemple.serie()))
 #         print(exemple.quan(i))
 
-z={i:randint(0,10) for i in range(100)}
-exemple=Stat(tuple([i for i in range(11)]),p)
-print(sorted(exemple.serie()))
-print(exemple.quan(3))
+# exemple=Stat(tuple([i for i in range(11)]),p)
+# print(sorted(exemple.serie()))
+# print(exemple.quan(3))
+print(exemple.ecart)
